4dc480f18e3eeb66621895d01f44d45e2d467f2b
cangothic <rockangel2910@gmail.com>
2015-11-02 13:38:32 -0500

    cambio de calse a archivo
    se cambio la clase por un archivo al ya existir un generador de eventos

--- mannanger/Algoritmos_de_manejo_de_dinero.py
+++ mannanger/Algoritmos_de_manejo_de_dinero.py
@@ -1,14 +1,10 @@
 __author__ = 'rocka'
-class Algoritmos_de_manejo_de_dienero:
-    def __int__(self):
-        object.__init__()
+def transferencia(jugador,agente):
+    if(jugador.saldo > 200):
+        jugador.saldo-=200
+        agente.dinero_recaudado+=200
 
-    def transferencia(self,jugador,agente):
-        if(jugador.saldo > 200):
-            jugador.saldo-=200
-            agente.dinero_recaudado+=200
-
-    def comision_de_agente(self,jugador,agente):
-        jugador.saldo+=agente.dinero_recaudado
-        agente.dinero_recaudado=0
+def comision_de_agente(jugador,agente):
+    jugador.saldo+=agente.dinero_recaudado
+    agente.dinero_recaudado=0
 
