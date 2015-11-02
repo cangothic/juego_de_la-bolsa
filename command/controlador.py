355a4143324ff9ac742f8a184f3c7dd066f01b25
cangothic <rockangel2910@gmail.com>
2015-11-02 13:41:12 -0500

    eliminacion de metodos en controlador
    al ya existir la clase generador de eventos estos metodos ya no son
    necesarios

--- command/controlador.py
+++ command/controlador.py
@@ -1,17 +1,9 @@
 __author__ = 'rocka'
 from vista.Interfas_de_juego import *
 from vista.Configuracion_del_juego import *
-from  mannanger.Algoritmos_de_manejo_de_dinero import *
 #recibe peticiones y llama a lo que nesecite
 def recibir_peticiones(peticion):
     if(peticion=="comenzar"):
         pregunta=Configuracion_deljuego()
     if(peticion=="juego"):
         iniciar_juego(0)
-def transferencia(jugador,agente):
-    manejo=Algoritmos_de_manejo_de_dienero()
-    manejo.transferencia(jugador,agente)
-
-def comision_de_agente(jugador,agente):
-    manejo=Algoritmos_de_manejo_de_dienero()
-    manejo.comision_de_agente(jugador,agente)
